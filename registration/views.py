28879dd93fe98d0ce6fefee030ab351abf7b585a
nebneb200411 <watanabenabeyuya@gmail.com>
2021-05-19 23:10:24 +0900

    follow関数，unfollow関数の実装．次はhtmlを作成して動くか確認.作成する際はuser_find.htmlなどとし，まずユーザー関数が動くか確認する．その次にユーザー検索機能を追加する

--- registration/views.py
+++ registration/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ProfileForm
 from django.contrib import messages
 from article.models import Article
-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 # SigUpのためのViewを作成
 
 
@@ -75,3 +76,49 @@ class UserProfileDetailView(DetailView, LoginRequiredMixin):
 class FriendAddView(TemplateView, LoginRequiredMixin):
     template_name = 'profile/other_user_profile.html'
     model = Follow
+
+
+@login_required
+def follow_view(request, *args, **kwargs):
+    try:
+        follower = User.objects.get(username=request.user.username)
+        followee = User.objects.get(username=kwargs['username'])
+
+    except User.DoesNotExist:
+        messages.warning(request, '{}は存在しません'.format(kwargs['username']))
+        return HttpResponseRedirect(reverse_lazy('index'))
+
+    if follower == followee:
+        messages.warning(request, '自分自身はフォローできません')
+
+    else:
+        _, created = Follow.objects.get_or_create(
+            follower=follower, followee=followee)
+
+        if (created):
+            messages.success(
+                request, 'あなたはすでに{}をフォローしています'.format(followee.username))
+        else:
+            messages.warning(
+                request, 'あなたはすでに{}をフォローしています'.format(followee.username))
+
+    return HttpResponseRedirect(reverse_lazy('index'), kwargs={'username': followee.username})
+
+
+@login_required
+def unfollow_view(request, *args, **kwargs):
+    try:
+        follower = User.objects.get(username=request.username)
+        followee = User.objects.get(username=request.username)
+        if follower == followee:
+            messages.warning('自分自身のフォローははずせません')
+        else:
+            unfollow = Follow.objects.get(follower=follower, followee=followee)
+            unfollow.delete()
+            messages.success(
+                request, '{}のフォローを外しました'.format(followee.username))
+    except User.DoesNotExist:
+        messages.warning(request, '{}は存在しません'.format(kwargs['username']))
+        return HttpResponseRedirect(reverse_lazy('index'))
+
+    return HttpResponseRedirect(reverse_lazy('index'), kwargs={'username': followee.username})
