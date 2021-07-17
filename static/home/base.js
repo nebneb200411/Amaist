91c4f589d7533a35104fa8a21be07dadb694dd7f
nebneb200411 <watanabenabeyuya@gmail.com>
2021-07-17 22:00:30 +0900

    ログイン画面は満足だが，要素の位置がおかしい，ログインの方法は，絶対にメールアドレスにしたい．

--- static/home/base.js
+++ static/home/base.js
@@ -34,18 +34,12 @@ window.addEventListener('DOMContentLoaded', function () {
 
 );
 
-//ナビゲーションより下に対してはナビゲーションの高さを考慮した位置に配置
-//ナビゲーションの高さ取得
-//let navigation_bar_height = document.getElementById("header-nav-box").clientHeight;
-//位置および高さの変更
-//var element = document.getElementById("under-header");
-//element.style.height = hsize - navigation_bar_height + "px";
-//element.style.top = navigation_bar_height + "px";
-
 //メニューボタンクリック時の処理
 $('.menu-button').on('click', function () {
-    $('.menu-button').toggleClass('clicked');
     $('.header-nav').toggleClass('slide-in');
+    $('.top-line').toggleClass('clicked-top');
+    $('.middle-line').toggleClass('clicked-middle');
+    $('.bottom-line').toggleClass('clicked-bottom');
 }
 
 );
