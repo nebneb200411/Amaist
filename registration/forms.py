9766f54dc036343d01cbdef4b5ff55318b4d898d
nebneb200411 <watanabenabeyuya@gmail.com>
2021-08-02 17:56:46 +0900

    メールのurlには成功したが，送ったらエラーページに遷移する．登録，アクティベート化には問題なし

--- registration/forms.py
+++ registration/forms.py
@@ -2,10 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.conf import settings
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django import forms
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.auth.tokens import default_token_generator
 # load templates
 from django.template.loader import render_to_string
 # send email
@@ -15,47 +15,7 @@ from django.core.mail import send_mail
 User = get_user_model()
 
 
-def get_activate_url(user):
-    uid = urlsafe_base64_encode(force_bytes(user.pk))
-    token = default_token_generator.make_token(user)
-    return str(settings.BASE_DIR) + "/activate/{}/{}/".format(uid, token)
-
-
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.is_active = False
-        if commit:
-            user.save()
-            activate_url = get_activate_url(user)
-            subject = "ユーザー登録について"
-            context = {
-                "username": user.username,
-                "email": user.email,
-                "register_link": activate_url,
-            }
-            message = render_to_string('email/register.txt', context)
-            user.email_user(subject, message)
-        return user
-
-# ユーザー有効化の設定
-
-
-def activate_user(uidb64, token):
-    try:
-        uid = urlsafe_base64_decode(uidb64).decode()
-        user = User.objects.get(pk=uid)
-    except Exception:
-        return False
-
-    if default_token_generator.check_token(user, token):
-        user.is_active = True
-        user.save()
-        return True
-
-    return False
--- registration/views.py
+++ registration/views.py
@@ -4,10 +4,22 @@ from django.urls import reverse_lazy
 from .forms import SignUpForm
 from django.views.generic import DeleteView
 from django.contrib.auth import get_user_model
-from .forms import activate_user
 from django.contrib.auth.views import PasswordResetView
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.auth.tokens import default_token_generator
+# load templates
+from django.template.loader import render_to_string
+# send email
+from django.core.mail import send_mail
 
 User = get_user_model()
+
+
+def get_activate_url(user):
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = default_token_generator.make_token(user)
+    return "/activate/{}/{}/".format(uid, token)
 
 
 class SignUpView(CreateView):
@@ -15,10 +27,44 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
+    def form_valid(self, form, commit=True):
+        user = form.save(commit=False)
+        #user.email = self.cleaned_data['email']
+        user.is_active = False
+        if commit:
+            user.save()
+            activate_url = str(
+                self.request._current_scheme_host) + get_activate_url(user)
+            subject = "ユーザー登録について"
+            context = {
+                "username": user.username,
+                "email": user.email,
+                "register_link": activate_url,
+            }
+            message = render_to_string('email/register.txt', context)
+            user.email_user(subject, message)
+            return user
+        return super().form_valid(form)
+
 
 class UserDeleteView(DeleteView):
     model = User
     success_url = reverse_lazy('login')
+
+
+def activate_user(uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+    except Exception:
+        return False
+
+    if default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return True
+
+    return False
 
 
 class ActivateView(TemplateView):
