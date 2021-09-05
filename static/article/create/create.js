50a4e1a5fd06899b432bfd0442658c8fe9823688
nebneb200411 <watanabenabeyuya@gmail.com>
2021-09-05 20:39:47 +0900

    create_article design was changed

--- static/article/create/create.js
+++ static/article/create/create.js
@@ -1,3 +1,8 @@
 $('#tag-add-button').click(function () {
     $('#tag-box').append('<input type="text" multiple name="tags" class="tag-input" placeholder="&#xf02b;tag"/>')
 })
+
+$(function () {
+    var page_title_image = $('#page-title-box');
+    page_title_image.height(page_title_image.width() * 0.4);
+})
