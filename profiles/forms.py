2d7a51e615bff5b643e16569426725f4064d3172
nebneb200411 <watanabenabeyuya@gmail.com>
2021-12-12 18:42:25 +0900

    profile update has changed

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,6 +7,6 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ('icon', 'introduction')
         widgets = {
-            'icon': forms.FileInput(attrs={'class': 'icon'}),
-            'introduction': forms.TextInput(attrs={'class': 'introduction'}),
+            'icon': forms.FileInput(attrs={'class': 'align-self-center'}),
+            'introduction': forms.TextInput(attrs={'class': 'align-self-center'}),
         }
