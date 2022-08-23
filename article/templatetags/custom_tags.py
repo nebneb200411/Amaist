31d1df1e131bc49e64723b47f04c21b1ae311bba
nebneb200411 <72803255+nebneb200411@users.noreply.github.com>
2022-08-23 17:20:36 +0900

    list good num

--- article/templatetags/custom_tags.py
+++ article/templatetags/custom_tags.py
@@ -5,3 +5,9 @@ register = template.Library()
 @register.filter
 def sort_by(queryset, order):
     return queryset.order_by('-created_at')
+
+@register.simple_tag
+def count_good(article):
+    evaluators = article.good_from.all()
+    good_num = evaluators.count()
+    return good_num
